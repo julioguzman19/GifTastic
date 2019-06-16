b04253b166f06fa775b5b8412ab1e404bcb0d9d2
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-06-16 12:25:37 -0600

    Search Value being retrieved and giphy outputting

--- javascript.js
+++ javascript.js
@@ -1,20 +1,21 @@
 /* let queryURL = "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=cats"; */
 //Using my API Key
-let api = "http://api.giphy.com/v1/gifs/search?"
-let apiKey = "&api_key=vuQllzX4miPlYbuVJ1MIViy7wMvNFuKw"
-let query = "&q=rainbow";
-let queryURL = (api + apiKey + query);
-//add-to-do ID on button
 
-$('#inputSearch').on("click",function(){
-    let giphy = document.getElementById("inputSearch").value 
-    console.log(giphy);
+$('#button').on("click",function(){
+    
+    
+    let api = "http://api.giphy.com/v1/gifs/search?"
+    let apiKey = "&api_key=vuQllzX4miPlYbuVJ1MIViy7wMvNFuKw"
+    let query = "&q=";//after q= its the search for the giphy
+    let giphyTextSearch = document.getElementById("inputSearch").value 
+    let queryURL = (api + apiKey + query + "'" + giphyTextSearch + "'");
+    console.log(giphyTextSearch);
+
     $.ajax({
         url:queryURL,
         method:"GET"
     }).then(function(response){
-        console.log(response)
-        $("#gif").append(response.data[0].imagess)
+        $("#gif").append("<img src = "+response.data[0].images.original.url+"/>")
     })
     
 })
