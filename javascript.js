fa3628a7c042cc11a704f2254c091774ce158d20
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-06-15 19:48:07 -0600

    Input form

--- javascript.js
+++ javascript.js
@@ -1,13 +1,10 @@
 /* let queryURL = "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=cats"; */
 //Using my API Key
-let queryURL = "https://api.giphy.com/v1/gifs/random?api_key=zxcvbnm123456789&tag=cats";
-
-$('$search').on("click",function(){
-
-    let queryURL = "https://api.giphy.com/v1/gifs/random?api_key=zxcvbnm123456789&tag=cats";
-
-    $.ajax({
-        url:queryURL,
-        method:"GET"
-    })
+let api = "http://api.giphy.com/v1/gifs/search?"
+let apiKey = "&api_key=vuQllzX4miPlYbuVJ1MIViy7wMvNFuKw"
+let query = "&q=rainbow";
+let queryURL = api + apiKey + query
+console.log("df")
+$('#add-to-do').on("click",function(){
+    console.log("df")
 })
