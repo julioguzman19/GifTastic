7b47e37bf8f11b8261db70c38d1d717712e20f2f
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-06-16 20:24:51 -0600

    New button animal failing for click on logic to be ran

--- javascript.js
+++ javascript.js
@@ -5,24 +5,33 @@ let animalList = ["Bat","Cat","Dog","Elephant","Pengin","Snail","Snake","Owl"];
     for(let i=0; i<animalList.length;i++){
         $(".animalButtons").append("<button "+ "data-animal =" +animalList[i] +">"  + animalList[i] + "</button>")
     }
+
 //Creating new butons for users inputs and appending into id animalButtons
 $('#button').on("click",function(){
     let giphyTextSearch = document.getElementById("inputSearch").value
     $(".animalButtons").append("<button "+ "data-animal =" +giphyTextSearch +">" + giphyTextSearch  + "</button>")
+    console.log(giphyTextSearch)
 })
 
 //Retrieving Giphys via button click
 $('button').on("click",function(){
     let currentAnimal = $(this).attr("data-animal");
-    console.log(currentAnimal);
-    
-    let api = "http://api.giphy.com/v1/gifs/search?"
-    let apiKey = "&api_key=vuQllzX4miPlYbuVJ1MIViy7wMvNFuKw"
-    let query = "&q=";//after q= its the search for the giphy
-    let giphyTextSearch = document.getElementById("inputSearch").value 
-    let queryURL = (api + apiKey + query + "'" + giphyTextSearch + "'");
+    if(currentAnimal!== undefined){
+        console.log(currentAnimal);
+        
+        let api = "http://api.giphy.com/v1/gifs/search?"
+        let apiKey = "&api_key=vuQllzX4miPlYbuVJ1MIViy7wMvNFuKw"
+        let query = "&q=";//after q= its the search for the giphy
+        let queryURL = (api + apiKey + query + "'" + currentAnimal + "'");
 
-  
+        $.ajax({
+            url:queryURL,
+            method:"GET"
+        }).then(function(response){
+            $("#gif").append("<img src = "+response.data[0].images.original.url+"/>")
+        }) 
+
+}
 })
 
 
