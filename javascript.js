b6618a3214e7ec34c7ff8e8e82793a00981f42a1
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-06-17 19:58:56 -0600

    Initializing on click State of giphy

--- javascript.js
+++ javascript.js
@@ -1,55 +1,49 @@
 //Array for current visible buttons
-let animalList = ["Bat","Cat","Dog","Elephant","Pengin","Snail","Snake","Owl"];
+let animalList = ["Cat","Dog","Elephant","Pengin","Snail","Snake","Owl","Sloth"];
 
 //Creating buttons for current animal list
     for(let i=0; i<animalList.length;i++){
-        $(".animalButtons").append("<button "+ "data-animal =" +animalList[i] +">"  + animalList[i] + "</button>")
+        $(".animalButtons").append("<button "+ "data-animal =" + "data-state ="+ +animalList[i] +">"  + animalList[i] + "</button>")
     }
 
 //Creating new butons for users inputs and appending into id animalButtons
 $('#button').on("click",function(){
     let giphyTextSearch = document.getElementById("inputSearch").value
-    $(".animalButtons").append("<button "+ "data-animal =" +giphyTextSearch +">" + giphyTextSearch  + "</button>")
-    console.log(giphyTextSearch)
+    $(".animalButtons").append("<button "+ "data-animal =" + "data-state =" +giphyTextSearch +">" + giphyTextSearch  + "</button>")
+  
+    animalList.push(giphyTextSearch);
+    
 })
+
+//FAILING CUZ THE ABOVE IS NOT BEING RECOGNIZED GLOBALLY? FOR HTE NEW BUTTON
 
 //Retrieving Giphys via button click
 $('button').on("click",function(){
     let currentAnimal = $(this).attr("data-animal");
     if(currentAnimal!== undefined){
-        console.log(currentAnimal);
+       
         
         let api = "http://api.giphy.com/v1/gifs/search?"
         let apiKey = "&api_key=vuQllzX4miPlYbuVJ1MIViy7wMvNFuKw"
         let query = "&q=";//after q= its the search for the giphy
         let queryURL = (api + apiKey + query + "'" + currentAnimal + "'");
 
-        $.ajax({
-            url:queryURL,
-            method:"GET"
-        }).then(function(response){
-            $("#gif").append("<img src = "+response.data[0].images.original.url+"/>")
-        }) 
-
+        for(let i=0; i<10;i++){
+            $.ajax({
+                url:queryURL,
+                method:"GET"
+            }).then(function(response){
+                //original will have animated original_still wont be animated can set variable for this
+                $("#gif").append("<img src = "+response.data[i].images.original_still.url+"/>")
+                
+            }) 
+        }   
 }
+
 })
 
+$('#gif').on("click",function(e){
+    console.log(e);
 
+})
 
-/* $('#button').on("click",function(){
-    
-    
-    let api = "http://api.giphy.com/v1/gifs/search?"
-    let apiKey = "&api_key=vuQllzX4miPlYbuVJ1MIViy7wMvNFuKw"
-    let query = "&q=";//after q= its the search for the giphy
-    let giphyTextSearch = document.getElementById("inputSearch").value 
-    let queryURL = (api + apiKey + query + "'" + giphyTextSearch + "'");
-    console.log(giphyTextSearch);
-
-    $.ajax({
-        url:queryURL,
-        method:"GET"
-    }).then(function(response){
-        $("#gif").append("<img src = "+response.data[0].images.original.url+"/>")
-    }) 
-}) */
