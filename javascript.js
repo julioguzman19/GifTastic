a2d8fa378a98aeeac1c2a55fe280f5512c513410
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-06-19 21:33:48 -0600

    Rating added Successful

--- javascript.js
+++ javascript.js
@@ -28,9 +28,10 @@ $(".animalButtons").on("click",".gif",function(){
         let apiKey = "&api_key=vuQllzX4miPlYbuVJ1MIViy7wMvNFuKw"
         let query = "&q=";//after q= its the search for the giphy
         let queryURL = (api + apiKey + query + "'" + currentAnimal + "'");
-        
+        $("#gifsDiv").empty();
+        $("#gifsDiv").empty()
 
-        for(let i=0; i<1;i++){
+        for(let i=0; i<10;i++){
             $.ajax({
                 url:queryURL,
                 method:"GET"
@@ -38,10 +39,11 @@ $(".animalButtons").on("click",".gif",function(){
                 //original will have animated original_still wont be animated can set variable for this
                 let still = response.data[i].images.original_still.url;
                 let animate = response.data[i].images.original.url
-                // let img = `<img class="gif" src=${still} data-number=${i} />`
-                let img = `<img 
-                class="gif" src=${still} data-animate=${animate} data-still=${still} data-state="still" />`
-                $("#gifsDiv").empty().append(img); //empty will empty element so then i can just replace
+                let rating = $("<p>").text("Rating: " +response.data[i].rating);
+                let img = `<img class="gif" src=${still} data-animate=${animate} data-still=${still} data-state="still" />`
+                
+                $("#gifsDiv").append(rating);
+                $("#gifsDiv").append(img); //empty will empty element so then i can just replace
             }) 
         }   
     }
