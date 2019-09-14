8b24dc8626b17751a780461c610fd005f37b21f5
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-09-14 17:51:03 -0600

    added text and moved search input down

--- javascript.js
+++ javascript.js
@@ -1,67 +1,85 @@
 //Array for current visible buttons
-let list = ["Cat","Sloth","Funny","Basketball","Coder","GameofThrones","Netflix","Bachelor","Team","Tequila"];
+let list = [
+  "Cat",
+  "Sloth",
+  "Funny",
+  "Basketball",
+  "Coder",
+  "GameofThrones",
+  "Netflix",
+  "Bachelor",
+  "Team",
+  "Tequila"
+];
 
 //Creating buttons for current animal list
-    for(let i=0; i<list.length;i++){
-        $(".animalButtons").append("<button "+"class = gif"+" " + "data-animal ="+ list[i]  +">"  + list[i] + "</button>")
-    }
+for (let i = 0; i < list.length; i++) {
+  $(".animalButtons").append(
+    "<button " +
+      "class = gif" +
+      " " +
+      "data-animal =" +
+      list[i] +
+      ">" +
+      list[i] +
+      "</button>"
+  );
+}
 
 //Creating new butons for users inputs and appending into id animalButtons
-$('#button').on("click",function(){
-    let giphyTextSearch = document.getElementById("inputSearch").value
-    $(".animalButtons").append("<button "+ "class = gif"+" " + "data-animal ="  +giphyTextSearch +">" + giphyTextSearch  + "</button>")
-    console.log(giphyTextSearch)
-    
-})
-
-
+$("#button").on("click", function() {
+  let giphyTextSearch = document.getElementById("inputSearch").value;
+  $(".animalButtons").append(
+    "<button " +
+      "class = gif" +
+      " " +
+      "data-animal =" +
+      giphyTextSearch +
+      ">" +
+      giphyTextSearch +
+      "</button>"
+  );
+});
 
 //Retrieving Giphys via button click
 //Third parameter to grab it from the parent for new buttons
-$(".animalButtons").on("click",".gif",function(){
-    let currentAnimal = $(this).attr("data-animal");
+$(".animalButtons").on("click", ".gif", function() {
+  let currentAnimal = $(this).attr("data-animal");
 
-    if(currentAnimal!== undefined){
-       
-        
-        let api = "https://api.giphy.com/v1/gifs/search?"
-        let apiKey = "&api_key=vuQllzX4miPlYbuVJ1MIViy7wMvNFuKw"
-        let query = "&q=";//after q= its the search for the giphy
-        let queryURL = (api + apiKey + query + "'" + currentAnimal + "'");
-        $("#gifsDiv").empty();
-        $("#gifsDiv").empty()
+  if (currentAnimal !== undefined) {
+    let api = "https://api.giphy.com/v1/gifs/search?";
+    let apiKey = "&api_key=vuQllzX4miPlYbuVJ1MIViy7wMvNFuKw";
+    let query = "&q="; //after q= its the search for the giphy
+    let queryURL = api + apiKey + query + "'" + currentAnimal + "'";
+    $("#gifsDiv").empty();
+    $("#gifsDiv").empty();
 
-        for(let i=0; i<10;i++){
-            $.ajax({
-                url:queryURL,
-                method:"GET"
-            }).then(function(response){
-                //original will have animated original_still wont be animated can set variable for this
-                let still = response.data[i].images.original_still.url;
-                let animate = response.data[i].images.original.url
-                let rating = ("Rating: " +response.data[i].rating);
-                let img = `<img class="gif" src=${still} data-animate=${animate} data-still=${still} data-state="still" />`
-                let div = `<div class="inlineBlock"><p> ${rating} </p><img class="gif" src= ${still} data-animate=${animate} data-still=${still} data-state="still" />   </div>`
-                
-                $("#gifsDiv").append(div);
-              
-            }) 
-        }   
+    for (let i = 0; i < 10; i++) {
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        //original will have animated original_still wont be animated can set variable for this
+        let still = response.data[i].images.original_still.url;
+        let animate = response.data[i].images.original.url;
+        let img = `<img class="gif" src=${still} data-animate=${animate} data-still=${still} data-state="still" />`;
+        let div = `<div class="inlineBlock"><p>  </p><img class="gif" src= ${still} data-animate=${animate} data-still=${still} data-state="still" />   </div>`;
+
+        $("#gifsDiv").append(div);
+      });
     }
+  }
 });
 
 //Animating on click
 
-$("#gifsDiv").on("click",".gif",function(event){
-    let state = $(this).attr("data-state");
-    if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
+$("#gifsDiv").on("click", ".gif", function(event) {
+  let state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
 });
-
-
-
